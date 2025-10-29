871b25fbc9eb307d70f6f89dc929d230222030d7
reyynoah <noahrey74@gmail.com>
2025-10-29 15:11:55 +0700

    Add Feature Edit Student

--- app/Http/Controllers/Admin/StudentController.php
+++ app/Http/Controllers/Admin/StudentController.php
@@ -55,18 +55,31 @@ class StudentController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(string $id)
+    public function edit(Student $student)
     {
-        //
+    return view('admin.student.edit', compact('student'));
     }
+
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, Student $student)
     {
-        //
+    $validated = $request->validate([
+        'nis'           => 'required',
+        'nama_lengkap'  => 'required',
+        'jenis_kelamin' => 'required',
+        'nisn'          => 'required',
+    ]);
+
+    $student->update($validated);
+
+    return redirect()
+        ->route('admin.students.index')
+        ->with('success', 'Data siswa berhasil diperbarui');
     }
+
 
     /**
      * Remove the specified resource from storage.

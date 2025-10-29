c8cc9bcd82dc3335028e83032138b122ba3a13c7
reyynoah <noahrey74@gmail.com>
2025-10-29 14:49:39 +0700

    Add Feature Create Student

--- app/Http/Controllers/Admin/StudentController.php
+++ app/Http/Controllers/Admin/StudentController.php
@@ -23,16 +23,26 @@ class StudentController extends Controller
      */
     public function create()
     {
-        //
+    return view('admin.student.create');
     }
+
 
     /**
      * Store a newly created resource in storage.
      */
     public function store(Request $request)
     {
-        //
+    $request->validate([
+        'nis' => 'required|unique:students',
+        'nama_lengkap' => 'required',
+        'jenis_kelamin' => 'required',
+        'nisn' => 'required|unique:students',
+    ]);
+
+    Student::create($request->all());
+    return redirect()->route('admin.students.index')->with('success', 'Data berhasil disimpan!');
     }
+
 
     /**
      * Display the specified resource.

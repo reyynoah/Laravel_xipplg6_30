e283f0c9ce9507fe54c855568b67bba044bf83fd
reyynoah <noahrey74@gmail.com>
2025-10-29 15:29:21 +0700

    Add Feature Show Student by ID

--- app/Http/Controllers/Admin/StudentController.php
+++ app/Http/Controllers/Admin/StudentController.php
@@ -47,10 +47,11 @@ class StudentController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(Student $student)
     {
-        //
+    return view('admin.student.show', compact('student'));
     }
+
 
     /**
      * Show the form for editing the specified resource.
